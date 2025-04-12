00bad3e641d7c3b4dac80b790db188800031531a
Philipp Runge <p.runge@progani.com>
2025-04-12 16:37:04 +0200

    add discord provider to next-auth

--- src/env.ts
+++ src/env.ts
@@ -1,15 +1,19 @@
 import { createEnv } from "@t3-oss/env-nextjs";
-// import { z } from "zod";
+import { z } from "zod";
 
 export const env = createEnv({
   server: {
     // DATABASE_URL: z.string().url(),
+    DISCORD_CLIENT_ID: z.string().min(1),
+    DISCORD_CLIENT_SECRET: z.string().min(1),
   },
   client: {
     // NEXT_PUBLIC_PUBLISHABLE_KEY: z.string().min(1),
   },
   runtimeEnv: {
     // DATABASE_URL: process.env.DATABASE_URL,
+    DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
+    DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
     // NEXT_PUBLIC_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_PUBLISHABLE_KEY,
   },
 });
--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -1,6 +1,18 @@
 import { NextAuthConfig } from "next-auth";
+import Discord from "next-auth/providers/discord";
+import { env } from "~/env";
 
 const authConfig: NextAuthConfig = {
-  providers: [],
+  providers: [
+    Discord({
+      clientId: env.DISCORD_CLIENT_ID,
+      clientSecret: env.DISCORD_CLIENT_SECRET,
+      authorization: {
+        params: {
+          scope: "identify email",
+        },
+      },
+    }),
+  ],
 };
 export default authConfig;
