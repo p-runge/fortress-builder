3c72c65592ba94c0ccf57d7bb20a3845000d91ea
Philipp Runge <p.runge@progani.com>
2025-04-13 22:58:58 +0200

    set up prisma with next auth adapter to it

--- src/env.ts
+++ src/env.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 
 export const env = createEnv({
   server: {
-    // DATABASE_URL: z.string().url(),
+    DATABASE_URL: z.string().url(),
     DISCORD_CLIENT_ID: z.string().min(1),
     DISCORD_CLIENT_SECRET: z.string().min(1),
   },
@@ -11,7 +11,7 @@ export const env = createEnv({
     // NEXT_PUBLIC_PUBLISHABLE_KEY: z.string().min(1),
   },
   runtimeEnv: {
-    // DATABASE_URL: process.env.DATABASE_URL,
+    DATABASE_URL: process.env.DATABASE_URL,
     DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
     DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
     // NEXT_PUBLIC_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_PUBLISHABLE_KEY,
--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -1,8 +1,11 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import { NextAuthConfig } from "next-auth";
 import Discord from "next-auth/providers/discord";
 import { env } from "~/env";
+import { db } from "../db";
 
 const authConfig: NextAuthConfig = {
+  adapter: PrismaAdapter(db),
   providers: [
     Discord({
       clientId: env.DISCORD_CLIENT_ID,
