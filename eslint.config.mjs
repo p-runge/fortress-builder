987f851778b7ff6c8eb53ef83a71a27e77c24335
Philipp Runge <p.runge@progani.com>
2025-04-15 16:42:29 +0200

    exclude prisma client from linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,9 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ignores: ["src/server/db/client"],
+  },
 ];
 
 export default eslintConfig;
