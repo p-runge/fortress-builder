d1386c684282225af22e0fdffd84a34a9a9ecd5a
Philipp Runge <p.runge@progani.com>
2025-04-16 02:44:52 +0200

    add auth to endpoints

--- src/server/api/routers/building.tsx
+++ src/server/api/routers/building.tsx
@@ -1,7 +1,7 @@
-import { db } from "~/server/db";
-import { publicProcedure, router } from "../trpc";
 import { z } from "zod";
+import { db } from "~/server/db";
 import { BuildingType } from "~/server/db/client";
+import { authedProcedure, router } from "../trpc";
 
 export const BuildingSchema = z.object({
   id: z.string(),
@@ -9,18 +9,23 @@ export const BuildingSchema = z.object({
 });
 
 export const buildingRouter = router({
-  getAll: publicProcedure.output(z.array(BuildingSchema)).query(async () => {
-    return await db.building.findMany();
-  }),
+  getAll: authedProcedure
+    .output(z.array(BuildingSchema))
+    .query(async ({ ctx: { session } }) => {
+      return await db.building.findMany({
+        where: {
+          userId: session.user.id,
+        },
+      });
+    }),
 
-  add: publicProcedure
+  add: authedProcedure
     .input(z.object({ type: BuildingSchema.shape.type }))
-    .mutation(async ({ input }) => {
-      console.log("Adding building", input);
+    .mutation(async ({ input, ctx: { session } }) => {
       return await db.building.create({
         data: {
           type: input.type,
-          userId: "", // Replace with actual user ID
+          userId: session.user.id,
         },
       });
     }),
--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -1,16 +1,64 @@
-import { initTRPC } from "@trpc/server";
+import { initTRPC, TRPCError } from "@trpc/server";
 import { cache } from "react";
 import { auth } from "../auth";
+import { z } from "zod";
+import { db } from "../db";
+
+const SessionSchema = z
+  .object({
+    user: z
+      .object({
+        id: z.string(),
+        email: z.string(),
+        image: z.string().optional(),
+        name: z.string(),
+      })
+      .strict(),
+    expires: z.string(),
+  })
+  .strict();
 
 export const createTRPCContext = cache(async () => {
   /**
    * @see: https://trpc.io/docs/server/context
    */
 
-  const session = auth();
+  const session = await auth();
+  let userId;
+
+  if (session?.user?.email) {
+    const data = await db.user.findUnique({
+      select: {
+        id: true,
+      },
+      where: {
+        email: session.user.email,
+      },
+    });
+    userId = data?.id;
+  }
+
+  const parsedSession = SessionSchema.safeParse(
+    session && userId
+      ? {
+          ...session,
+          user: {
+            ...session.user,
+            id: userId,
+          },
+        }
+      : null
+  );
+  if (!parsedSession.success) {
+    console.error("Session validation failed", parsedSession.error);
+    throw new TRPCError({
+      code: "INTERNAL_SERVER_ERROR",
+      message: "Session validation failed",
+    });
+  }
 
   return {
-    session,
+    session: parsedSession.data,
   };
 });
 
@@ -18,7 +66,7 @@ export const createTRPCContext = cache(async () => {
  * Initialization of tRPC backend
  * Should be done only once per backend!
  */
-const t = initTRPC.create();
+const t = initTRPC.context<typeof createTRPCContext>().create();
 /**
  * Export reusable router and procedure helpers
  * that can be used throughout the router
@@ -26,3 +74,19 @@ const t = initTRPC.create();
 export const router = t.router;
 export const publicProcedure = t.procedure;
 export const createCallerFactory = t.createCallerFactory;
+
+// procedure that asserts that the user is logged in
+export const authedProcedure = t.procedure.use(async function isAuthed(opts) {
+  const { ctx } = opts;
+  const { session } = ctx;
+
+  if (!session) {
+    throw new TRPCError({ code: "UNAUTHORIZED" });
+  }
+
+  return opts.next({
+    ctx: {
+      session,
+    },
+  });
+});
