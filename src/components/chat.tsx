71caa6460e47e7d14257a9b79a03ece87506dc8a
Marvin Demirkaya <m.demirkaya@progani.com>
2025-04-28 17:09:31 +0200

    change messsages array and implementation

--- src/components/chat.tsx
+++ src/components/chat.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { faComments } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -10,13 +12,16 @@ import {
 } from "./ui/sheet";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
+import { useSession } from "next-auth/react";
 
 export default function Chat() {
+  const { data: session } = useSession();
+
   const messages = [
-    "Test Message 1",
-    "Test Answer 1",
-    "Test Message 2",
-    "Test Answer 2",
+    { content: "Test Message 1", username: "Test User 1", sendAt: new Date() },
+    { content: "Test Message 2", username: "Test User 2", sendAt: new Date() },
+    { content: "Test Message 3", username: "Test User 3", sendAt: new Date() },
+    { content: "Test Message 4", username: "Test User 4", sendAt: new Date() },
   ];
 
   return (
@@ -28,28 +33,32 @@ export default function Chat() {
           className="m-2 cursor-pointer transition-transform hover:scale-110"
         />
       </SheetTrigger>
-      <SheetContent>
-        <SheetHeader>
-          <SheetTitle>Chat</SheetTitle>
-          <SheetDescription>
-            This is a global chat for Fortress Builder. Stay connected with your
-            friends and other people!
-          </SheetDescription>
-        </SheetHeader>
-        <div className="flex h-full flex-col gap-2">
-          <div className="grow">
-            {messages.map((message) => {
-              return (
-                <div key={message}>
-                  <span>{message}</span>
-                </div>
-              );
-            })}
+      {session && (
+        <SheetContent>
+          <SheetHeader>
+            <SheetTitle>Chat</SheetTitle>
+            <SheetDescription>
+              This is a global chat for Fortress Builder. Stay connected with
+              your friends and other people!
+            </SheetDescription>
+          </SheetHeader>
+          <div className="flex h-full flex-col gap-2">
+            <div className="grow">
+              {messages.map((message) => {
+                return (
+                  <div key={message.content}>
+                    <span>{message.content}</span>
+                    <div>{message.username}</div>
+                    <div>{message.sendAt.toISOString()}</div>
+                  </div>
+                );
+              })}
+            </div>
+            <Input />
+            <Button>Send</Button>
           </div>
-          <Input />
-          <Button>Send</Button>
-        </div>
-      </SheetContent>
+        </SheetContent>
+      )}
     </Sheet>
   );
 }
