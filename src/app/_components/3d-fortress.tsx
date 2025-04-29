458cb6f706ee4f73ab4d5f0afa606e8187e2b182
Philipp Runge <p.runge@progani.com>
2025-04-30 01:33:05 +0200

    render group of mocked hexagon fields on correct position

--- src/app/_components/3d-fortress.tsx
+++ src/app/_components/3d-fortress.tsx
@@ -1,15 +1,45 @@
 "use client";
 
-import { Canvas, useFrame } from "@react-three/fiber";
+import { Text } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
 import { useRef, useState } from "react";
 import { Mesh } from "three";
+import { FORTRESS_SIZE, getCoordinatesForSize } from "~/server/models/fortress";
+import { getCanvasPosition } from "~/utils/3d";
 
 export default function Fortress() {
+  // const { data: slots, isLoading } = api.fortress.getAllSlots.useQuery();
+
+  const coordinates = getCoordinatesForSize(FORTRESS_SIZE);
+  const slots = coordinates.map((coord) => ({
+    id: `${coord.x}/${coord.y}`,
+    x: coord.x,
+    y: coord.y,
+  }));
+
+  // if (isLoading) {
+  //   return null;
+  // }
+
+  if (!slots) {
+    return <div>No slots</div>;
+  }
+
   return (
-    <Canvas camera={{ position: [20, 20, 20], fov: 20 }}>
+    <Canvas camera={{ position: [0, 5, 5] }}>
       <ambientLight intensity={0.5} />
       <directionalLight position={[10, 10, 5]} />
-      <Box position={[0, 0, 0]} />
+      {slots.map((slot) => {
+        console.log(slot.x, slot.y, getCanvasPosition(slot.x, slot.y));
+        return (
+          <Box
+            key={slot.id}
+            position={getCanvasPosition(slot.x, slot.y)}
+            label={slot.id.toString()}
+            onClick={() => console.log("click")}
+          />
+        );
+      })}
     </Canvas>
   );
 }
@@ -20,18 +50,15 @@ function Box(props: {
   scale?: [number, number, number];
   color?: string;
   args?: [number, number, number];
+  label?: string;
   onClick?: () => void;
   onPointerOver?: () => void;
   onPointerOut?: () => void;
 }) {
-  // This reference will give us direct access to the mesh
   const meshRef = useRef<Mesh>(null);
-  // Set up state for the hovered and active state
   const [hovered, setHover] = useState(false);
   const [active, setActive] = useState(false);
-  // Subscribe this component to the render-loop, rotate the mesh every frame
-  useFrame((_state, delta) => (meshRef.current!.rotation.y += delta));
-  // Return view, these are regular three.js elements expressed in JSX
+
   return (
     <mesh
       position={props.position}
@@ -43,6 +70,18 @@ function Box(props: {
     >
       <cylinderGeometry args={[1, 1, 0.2, 6]} />
       <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+      {props.label && (
+        <Text
+          position={[0, 0.2, 0]} // Position the label slightly above the top face
+          fontSize={0.2}
+          rotation={[-Math.PI / 2, 0, 0]} // Rotate the text to face upwards
+          color="black"
+          anchorX="center"
+          anchorY="middle"
+        >
+          {props.label}
+        </Text>
+      )}
     </mesh>
   );
 }
---
+++ src/utils/3d.ts
@@ -0,0 +1,10 @@
+const HEX_SIZE = 1; // size of hexagon
+// transform coordinates of hexagonal map to canvas coordinates
+export function getCanvasPosition(
+  q: number,
+  r: number,
+): [number, number, number] {
+  const x = HEX_SIZE * Math.sqrt(3) * (q + r / 2);
+  const z = ((HEX_SIZE * 3) / 2) * r;
+  return [x, 0, z];
+}
