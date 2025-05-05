722f098f41c568d2a5ec51a9101d0c73116e1bcd
Philipp Runge <p.runge@progani.com>
2025-05-05 16:39:08 +0200

    add endpoint to receive pending contact requests

--- src/server/api/routers/user.tsx
+++ src/server/api/routers/user.tsx
@@ -1,6 +1,7 @@
 import { db } from "~/server/db";
 import { authedProcedure, router } from "../trpc";
 import { z } from "zod";
+import { RequestStatus } from "~/server/db/client";
 
 const UserSchema = z.object({
   id: z.string(),
@@ -27,4 +28,25 @@ export const userRouter = router({
         },
       });
     }),
+
+  getPendingContactRequestList: authedProcedure.query(
+    async ({ ctx: { session } }) => {
+      return db.contactRequest.findMany({
+        where: {
+          toId: session.user.id,
+          status: RequestStatus.PENDING,
+        },
+        select: {
+          id: true,
+          from: {
+            select: {
+              id: true,
+              name: true,
+              image: true,
+            },
+          },
+        },
+      });
+    },
+  ),
 });
