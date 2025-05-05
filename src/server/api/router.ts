06a8b7f72579e89f3e42823c9681e8a190724dcd
Philipp Runge <p.runge@progani.com>
2025-05-05 16:59:10 +0200

    move contact request endpoints to own router module

--- src/server/api/router.ts
+++ src/server/api/router.ts
@@ -4,11 +4,13 @@ import { itemRouter } from "./routers/item";
 import { paymentRouter } from "./routers/payment";
 import { resourceRouter } from "./routers/resource";
 import { userRouter } from "./routers/user";
+import { contactRequestRouter } from "./routers/contact-request";
 import { publicProcedure, router } from "./trpc";
 
 export const appRouter = router({
   ping: publicProcedure.query(() => "pong"),
   building: buildingRouter,
+  contactRequest: contactRequestRouter,
   fortress: fortressRouter,
   item: itemRouter,
   payment: paymentRouter,
