5608f621c804aabbb82cc80a185efac31ae76e60
Marvin Demirkaya <m.demirkaya@progani.com>
2025-05-09 11:52:42 +0200

    build contact dialog structure

---
+++ src/components/contact-dialog.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { faUserPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
+import { Dialog, DialogContent, DialogTrigger } from "~/components/ui/dialog";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+// import { api } from "~/api/client";
+
+export default function ContactDialog() {
+  const [open, setOpen] = useState(false);
+  let inputValue;
+  // const { data: contactList } = api.user.getContactList.useQuery(); //if user is already a friend, send request button will be disabled
+  // const {data: userList} = api. ; // Wait until endpoint is ready and fetch filtered array then
+
+  return (
+    <Dialog>
+      <DialogTrigger>
+        <FontAwesomeIcon icon={faUserPlus} onClick={() => setOpen(!open)} />
+      </DialogTrigger>
+      <DialogContent>
+        <div>
+          <div className="flex gap-x-2">
+            <Input
+              defaultValue="Search for a user..."
+              value={inputValue}
+            ></Input>
+            <Button>Search</Button>
+          </div>
+          {/* <div>{userList.map(()=>{})}</div> */}
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+}
