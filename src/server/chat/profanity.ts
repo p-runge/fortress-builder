050cfcdd94dff579e118a147abf96931a0d2fabe
Philipp Runge <p.runge@progani.com>
2025-05-12 16:52:48 +0200

    add custom black and whitelists

--- src/server/chat/profanity.ts
+++ src/server/chat/profanity.ts
@@ -2,4 +2,16 @@ import { Filter } from "bad-words";
 
 const profanityFilter = new Filter();
 
+profanityFilter.addWords(
+  ...[
+    // custom blacklist
+  ],
+);
+
+profanityFilter.removeWords(
+  ...[
+    // custom whitelist
+  ],
+);
+
 export { profanityFilter };
