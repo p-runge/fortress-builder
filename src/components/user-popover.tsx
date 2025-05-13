214a2b5dd4aa8cc33c196929d997ff0d8f3f994b
Philipp Runge <p.runge@progani.com>
2025-05-13 23:57:54 +0200

    add shadcn popover to user popover

--- src/components/user-popover.tsx
+++ src/components/user-popover.tsx
@@ -1,21 +1,50 @@
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "~/components/ui/popover";
 import { auth } from "~/server/auth";
 import { SignOutButton } from "./auth-buttons";
+import { Button } from "./ui/button";
+import { Separator } from "./ui/separator";
 
 export default async function UserPopover() {
   const session = await auth();
 
   return (
-    <div className="flex items-center gap-2">
-      <Image
-        src={session?.user?.image ?? ""}
-        alt="User Avatar"
-        className="h-8 w-8 rounded-full"
-        width={32}
-        height={32}
-      />
-      <span>{session?.user?.name}</span>
-      <SignOutButton />
-    </div>
+    <Popover>
+      <PopoverTrigger asChild>
+        <div className="flex items-center gap-2">
+          <Image
+            src={session?.user?.image ?? ""}
+            alt="User Avatar"
+            className="h-8 w-8 rounded-full"
+            width={32}
+            height={32}
+          />
+          <span>{session?.user?.name}</span>
+          <FontAwesomeIcon icon={faChevronDown} className="h-4 w-4" />
+        </div>
+      </PopoverTrigger>
+      <PopoverContent className="w-fit overflow-hidden p-0">
+        <ul className="grid">
+          <li>
+            {/* TODO: add user settings dialog */}
+            <Button variant="ghost" className="rounded-none">
+              Settings
+            </Button>
+          </li>
+          <li>
+            <Separator />
+          </li>
+          <li>
+            <SignOutButton />
+          </li>
+        </ul>
+      </PopoverContent>
+    </Popover>
   );
 }
