f5c6b21c2eed6280b068df5ae417f0a49747963b
Marvin Demirkaya <m.demirkaya@progani.com>
2025-05-15 12:56:20 +0200

    add styling for contact sheet and notification bell

--- src/components/contact-dialog.tsx
+++ src/components/contact-dialog.tsx
@@ -6,13 +6,16 @@ import { useState } from "react";
 import { Dialog, DialogContent, DialogTrigger } from "~/components/ui/dialog";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
-// import { api } from "~/api/client";
+import { api } from "~/api/client";
 
 export default function ContactDialog() {
   const [open, setOpen] = useState(false);
   let inputValue;
-  // const { data: contactList } = api.user.getContactList.useQuery(); //if user is already a friend, send request button will be disabled
-  // const {data: userList} = api. ; // Wait until endpoint is ready and fetch filtered array then
+  const { data: contactList } = api.user.getContactList.useQuery(); //if user is already a friend, send request button will be disabled
+  const { data: userList } = api.user.search.useQuery(
+    { name: inputValue },
+    { enabled: false },
+  );
 
   return (
     <Dialog>
@@ -23,7 +26,7 @@ export default function ContactDialog() {
         <div>
           <div className="flex gap-x-2">
             <Input
-              defaultValue="Search for a user..."
+              placeholder="Search for a user..."
               value={inputValue}
             ></Input>
             <Button>Search</Button>
