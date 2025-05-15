1e97e9f2e7e6fe09f4bed492cc45812e73b39719
Marvin Demirkaya <m.demirkaya@progani.com>
2025-05-15 12:56:20 +0200

    add styling for contact sheet and notification bell

--- src/components/notification-bell.tsx
+++ src/components/notification-bell.tsx
@@ -10,6 +10,7 @@ import {
 } from "@radix-ui/react-popover";
 import { api } from "~/api/client";
 import { Button } from "./ui/button";
+import Image from "next/image";
 
 export default function NotificationBell() {
   const [open, setOpen] = useState(false);
@@ -35,28 +36,60 @@ export default function NotificationBell() {
         <FontAwesomeIcon icon={faBell} onClick={() => setOpen(!open)} />
       </PopoverTrigger>
       <PopoverContent>
+        <span className="text-xl font-bold">New Notifications</span>
         {list?.map((listItem) => {
           return (
-            <div className="flex" key={listItem.id}>
-              <div className="flex gap-x-2">
-                <div>{listItem.from.image}</div>
-                <div>{listItem.from.name}</div>
-              </div>
-              <div className="flex flex-col gap-2">
-                <Button
-                  onClick={() => {
-                    acceptRequest({ id: listItem.id });
-                  }}
-                >
-                  <FontAwesomeIcon icon={faCheck} />
-                </Button>
-                <Button
-                  onClick={() => {
-                    rejectRequest({ id: listItem.id });
-                  }}
-                >
-                  <FontAwesomeIcon icon={faX} />
-                </Button>
+            <div
+              className="w-xs rounded border border-white bg-black p-2"
+              key={listItem.id}
+            >
+              <span className="text-xl font-bold">New Contact Request</span>
+              <div className="flex rounded border border-white p-2">
+                <div className="flex w-full items-center gap-x-2">
+                  <div className="flex items-center">
+                    {listItem.from.image ? (
+                      <Image
+                        alt="Discord User Profile Picture"
+                        src={listItem.from.image}
+                        width={60}
+                        height={60}
+                        className="rounded-full"
+                      ></Image>
+                    ) : (
+                      <Image
+                        alt="Default User Profile Picture"
+                        src="/default-profile-pic.png"
+                        width={60}
+                        height={60}
+                        className="rounded-full"
+                      ></Image>
+                    )}
+                    <div
+                      title={listItem.from.name}
+                      className="grow truncate text-xl"
+                    >
+                      {listItem.from.name}
+                    </div>
+                  </div>
+                </div>
+                <div className="flex flex-col gap-2">
+                  <Button
+                    className="bg-green-400"
+                    onClick={() => {
+                      acceptRequest({ id: listItem.id });
+                    }}
+                  >
+                    <FontAwesomeIcon icon={faCheck} />
+                  </Button>
+                  <Button
+                    className="bg-red-400"
+                    onClick={() => {
+                      rejectRequest({ id: listItem.id });
+                    }}
+                  >
+                    <FontAwesomeIcon icon={faX} />
+                  </Button>
+                </div>
               </div>
             </div>
           );
