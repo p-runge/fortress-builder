4cad703274e5b93f8c6eef0631f80290ae666bfe
Philipp Runge <p.runge@progani.com>
2025-05-15 12:27:50 +0200

    add skeleton-based loading state for fortress

--- src/app/_components/fortress.tsx
+++ src/app/_components/fortress.tsx
@@ -2,6 +2,10 @@
 
 import { Canvas } from "@react-three/fiber";
 import { api } from "~/api/client";
+import { CanvasHtml } from "~/components/canvas-html";
+import { Skeleton } from "~/components/ui/skeleton";
+import { FORTRESS_SIZE, getCoordinatesForSize } from "~/server/models/fortress";
+import { getCanvasPosition } from "~/utils/3d";
 import FortressField from "./fortress-field";
 import { useOverlays } from "./overlay-provider";
 
@@ -10,23 +14,19 @@ export default function Fortress() {
 
   const { overlays } = useOverlays();
 
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  if (!fields) {
-    return <div>No fields</div>;
-  }
-
   return (
     <div className="relative h-full w-full">
       <Canvas camera={{ position: [0, 5, 5] }}>
         <ambientLight intensity={0.5} />
         <directionalLight position={[10, 10, 5]} />
-        {fields.map((field) => {
-          // @ts-expect-error date props within building are incorrectly inferred by Zod
-          return <FortressField key={field.id} field={field} />;
-        })}
+        {isLoading ? (
+          <FortressSkeleton />
+        ) : (
+          fields?.map((field) => {
+            // @ts-expect-error date props within building are incorrectly inferred by Zod
+            return <FortressField key={field.id} field={field} />;
+          })
+        )}
       </Canvas>
       {/* overlay wrapper */}
       <div className="pointer-events-none absolute inset-0">
@@ -37,5 +37,42 @@ export default function Fortress() {
         ))}
       </div>
     </div>
+  );
+}
+
+const HEXAGON_SIZE = 160; // px
+const HEXAGON_SIDE_RELATION = 2 / Math.sqrt(3); // 1.1547
+
+function FortressSkeleton() {
+  const coordinates = getCoordinatesForSize(FORTRESS_SIZE);
+
+  return (
+    <CanvasHtml>
+      <div
+        className="relative h-full w-full"
+        style={{ transform: "perspective(650px) rotateX(45deg)" }}
+      >
+        {coordinates.map(({ x, y }, index) => {
+          const position = getCanvasPosition(x, y);
+
+          return (
+            <Skeleton
+              key={index}
+              className="-translate-1/2"
+              style={{
+                position: "absolute",
+                left: `${((position[0] * HEXAGON_SIDE_RELATION) / 2) * HEXAGON_SIZE}px`,
+                top: `${((position[2] * HEXAGON_SIDE_RELATION) / 2) * HEXAGON_SIZE}px`,
+                width: `${HEXAGON_SIZE}px`,
+                height: `${HEXAGON_SIZE * HEXAGON_SIDE_RELATION}px`,
+                // hexagon shape
+                clipPath:
+                  "polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%)",
+              }}
+            />
+          );
+        })}
+      </div>
+    </CanvasHtml>
   );
 }
