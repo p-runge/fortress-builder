db76d3a06183d4fdd0bdef635347b61dbe88cd4e
Marvin Demirkaya <m.demirkaya@progani.com>
2025-05-16 14:58:01 +0200

    add dialog title

--- src/components/contact-dialog.tsx
+++ src/components/contact-dialog.tsx
@@ -3,7 +3,12 @@
 import { faUserPlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
-import { Dialog, DialogContent, DialogTrigger } from "~/components/ui/dialog";
+import {
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  DialogTrigger,
+} from "~/components/ui/dialog";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
 import { api } from "~/api/client";
@@ -12,7 +17,7 @@ export default function ContactDialog() {
   const [open, setOpen] = useState(false);
   let inputValue;
   const { data: contactList } = api.user.getContactList.useQuery(); //if user is already a friend, send request button will be disabled
-  const { data: userList } = api.user.search.useQuery(
+  const { data: userList, refetch: refetchUser } = api.user.search.useQuery(
     { name: inputValue },
     { enabled: false },
   );
@@ -23,13 +28,25 @@ export default function ContactDialog() {
         <FontAwesomeIcon icon={faUserPlus} onClick={() => setOpen(!open)} />
       </DialogTrigger>
       <DialogContent>
+        <DialogTitle>Add new Contact</DialogTitle>
         <div>
           <div className="flex gap-x-2">
             <Input
               placeholder="Search for a user..."
               value={inputValue}
+              //Bei Klick auf Search hat der User ein Formular ausgefüllt, Formular hat ein onSumbit, onSubmit hat Zugriff auf die Daten der Formularfelder (input Feld)
+              //Vorlage in user-settings-dialog.tsx nach Rebase
+              //Kommentare nicht auf Deutsch
             ></Input>
-            <Button>Search</Button>
+            <Button
+              onClick={async () => {
+                try {
+                  await refetchUser();
+                } catch {}
+              }}
+            >
+              Search
+            </Button>
           </div>
           {/* <div>{userList.map(()=>{})}</div> */}
         </div>
