e7cf44615a189c61b49cd1eb176bd1c2b78c18ca
Marvin Demirkaya <m.demirkaya@progani.com>
2025-05-16 11:18:03 +0200

    finish styling for contact sheet and notification bell popover

--- src/components/notification-bell.tsx
+++ src/components/notification-bell.tsx
@@ -35,60 +35,59 @@ export default function NotificationBell() {
       <PopoverTrigger>
         <FontAwesomeIcon icon={faBell} onClick={() => setOpen(!open)} />
       </PopoverTrigger>
-      <PopoverContent>
-        <span className="text-xl font-bold">New Notifications</span>
+      <PopoverContent className="rounded border border-white bg-black p-2">
+        <div className="mb-2 block text-xl font-bold">New Notifications</div>
         {list?.map((listItem) => {
           return (
-            <div
-              className="w-xs rounded border border-white bg-black p-2"
-              key={listItem.id}
-            >
-              <span className="text-xl font-bold">New Contact Request</span>
-              <div className="flex rounded border border-white p-2">
-                <div className="flex w-full items-center gap-x-2">
-                  <div className="flex items-center">
-                    {listItem.from.image ? (
-                      <Image
-                        alt="Discord User Profile Picture"
-                        src={listItem.from.image}
-                        width={60}
-                        height={60}
-                        className="rounded-full"
-                      ></Image>
-                    ) : (
-                      <Image
-                        alt="Default User Profile Picture"
-                        src="/default-profile-pic.png"
-                        width={60}
-                        height={60}
-                        className="rounded-full"
-                      ></Image>
-                    )}
-                    <div
-                      title={listItem.from.name}
-                      className="grow truncate text-xl"
-                    >
-                      {listItem.from.name}
+            <div key={listItem.id}>
+              <div className="mb-2 flex flex-col rounded border border-white p-2">
+                <div className="text-xl font-bold">New Contact Request</div>
+                <div className="flex">
+                  <div className="flex items-center gap-x-2">
+                    <div className="flex w-xs items-center">
+                      {listItem.from.image ? (
+                        <Image
+                          alt="Discord User Profile Picture"
+                          src={listItem.from.image}
+                          width={60}
+                          height={60}
+                          className="shrink-0 rounded-full"
+                        ></Image>
+                      ) : (
+                        <Image
+                          alt="Default User Profile Picture"
+                          src="/default-profile-pic.png"
+                          width={60}
+                          height={60}
+                          className="shrink-0 rounded-full"
+                        ></Image>
+                      )}
+                      <div
+                        title={listItem.from.name}
+                        className="truncate text-xl"
+                      >
+                        {listItem.from.name}
+                      </div>
                     </div>
                   </div>
-                </div>
-                <div className="flex flex-col gap-2">
-                  <Button
-                    className="bg-green-400"
-                    onClick={() => {
-                      acceptRequest({ id: listItem.id });
-                    }}
-                  >
-                    <FontAwesomeIcon icon={faCheck} />
-                  </Button>
-                  <Button
-                    className="bg-red-400"
-                    onClick={() => {
-                      rejectRequest({ id: listItem.id });
-                    }}
-                  >
-                    <FontAwesomeIcon icon={faX} />
-                  </Button>
+                  <div className="flex flex-col gap-2">
+                    <Button
+                      className="bg-green-400"
+                      onClick={() => {
+                        acceptRequest({ id: listItem.id });
+                      }}
+                    >
+                      <FontAwesomeIcon icon={faCheck} />
+                    </Button>
+                    <Button
+                      className="bg-red-400"
+                      onClick={() => {
+                        rejectRequest({ id: listItem.id });
+                      }}
+                    >
+                      <FontAwesomeIcon icon={faX} />
+                    </Button>
+                  </div>
                 </div>
               </div>
             </div>
